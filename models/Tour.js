09ea2e9c7c89f14f68b65a28819b4a05c028c9ef
Tasnim Pricila <tasnimtanzim55@gmail.com>
2022-09-26 22:14:07 +0600

    Schema created

---
+++ models/Tour.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const tourSchema = mongoose.Schema({
+    place: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [3, 'Name must be at least 3 characters'],
+        maxLength: [30, 'Name is too long']
+    },
+    cost: {
+        type: Number,
+        required: true,
+        min: [0, 'Price can not be negative']
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    hotelName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [3, 'Name must be at least 3 characters'],
+        maxLength: [50, 'Name is too long']
+    },
+    groupSize: {
+        type: Number,
+        required: true,
+        min: [0, 'Group size can not be negative'],
+        max: [25, 'Member can not be more then 25']
+    },
+    guideName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [3, 'Name must be at least 3 characters'],
+        maxLength: [30, 'Name is too long']
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
