d22fb4c2122fe461e9b779ab230009622d3642dd
Tasnim Pricila <tasnimtanzim55@gmail.com>
2022-09-26 21:00:00 +0600

    file setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+    console.log(`Database connection is successful!!`.red.bold)
+})
+
+app.get('/', (req, res) => {
+    res.send("Tour is starting... !")
+})
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`.red.bold)
+})
